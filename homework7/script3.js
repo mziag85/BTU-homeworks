ed2fdfc32a538530b970c238ff3834263990b132
mziag85 <mziag85@gmail.com>
2026-09-08 11:21:50 +0400

    corrected

--- homework7/script3.js
+++ homework7/script3.js
@@ -26,28 +26,28 @@ while (i < arr.length) {
 }
 console.log(sum);
 
----
 
 
-const arr = [3, 7, -2, 10, 0, 5];
-let sum = 0;
-let i = 0;
+
+const arr2 = [3, 7, -2, 10, 0, 5];
+let sum2 = 0;
+let i2 = 0;
 
 do {
-  sum = sum + arr[i++];
-} while (i < arr.length);
-console.log(sum);
+  sum2 = sum2 + arr2[i2++];
+} while (i2 < arr2.length);
+console.log(sum2);
 
 
----
 
 
-const arr = [3, 7, -2, 10, 0, 5];
-let sum = 0;
+
+const arr3 = [3, 7, -2, 10, 0, 5];
+let sum3 = 0;
 
 for(let i = 0;  i< arr.length; i++) {
-  sum = sum + arr[i];
+  sum3 = sum3 + arr[i];
 }
-console.log(sum);
+console.log(sum3);
 
 
