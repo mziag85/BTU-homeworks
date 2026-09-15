bb2720feb9a029d29434bd95f812bf2f163589e3
mziag85 <mziag85@gmail.com>
2026-09-15 20:54:20 +0400

    added edit and delete

--- homework12/homework12.2/js2.js
+++ homework12/homework12.2/js2.js
@@ -4,19 +4,44 @@
 
 
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+
+const button_add = document.querySelector("button");
+
 const todo_list = document.querySelector(".todo-list");
 
-button.addEventListener("click", function(){
+button_add.addEventListener("click", function () {
 
-  if (input.value.trim() === "") {
-return;
-}
- const newTask = document.createElement("div");
- newTask.textContent = input.value;
- todo_list.appendChild(newTask);
+    if (input.value.trim() === "") {
+        return;
+    }
 
- input.value = ""
+    const newTask = document.createElement("div");
 
+    const taskText = document.createElement("span");
+    taskText.textContent = input.value;
 
+    newTask.appendChild(taskText);
+
+    const edit = document.createElement("button");
+    edit.textContent = "edit";
+
+    newTask.appendChild(edit);
+
+    edit.addEventListener("click", function () {
+      
+        input.value = taskText.textContent;
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+
+    newTask.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function () {
+        newTask.remove();
+    });
+
+    todo_list.appendChild(newTask);
+
+    input.value = "";
 });
